f10c5b1730f41d1e8d414eec881211fec783f603
ImDebabrata <debabratakgt@hotmail.com>
2023-03-01 02:31:50 +0530

    display user data

--- Frontend/src/Pages/User.jsx
+++ Frontend/src/Pages/User.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 import { FaUserAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
 
 const User = () => {
+  const { name, email, role } = useSelector((store) => store.auth);
   return (
     <Container>
       <Header>User Info</Header>
       <UserBox>
         <FaUserAlt />
-        <div>Name: Debabrata</div>
-        <div>Email: datta@gmail.com</div>
-        <div>Role: User</div>
+        <div>Name: {name}</div>
+        <div>Email: {email}</div>
+        <div>Role: {role}</div>
         <button>Update</button>
       </UserBox>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin: 35px 0;
+`;
 const Header = styled.h3``;
 const UserBox = styled.div`
   width: 350px;
   padding: 20px;
   margin: auto;
-  border: 1px solid black;
+  /* border: 1px solid black; */
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
   & > svg {
     border: 1px solid black;
     font-size: 5.5rem;
