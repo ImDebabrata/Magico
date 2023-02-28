fa1d4502b7e09ba28642f2cb804643437b44e1be
ImDebabrata <debabratakgt@hotmail.com>
2023-03-01 02:03:46 +0530

    send login user email

--- Backend/routes/login.route.js
+++ Backend/routes/login.route.js
@@ -22,8 +22,9 @@ loginRouter.post("/", async (req, res) => {
           res.send({
             res: `Hello ${isExist.name}`,
             name: isExist.name,
-            token,
             role: isExist.role,
+            email: isExist.email,
+            token,
           });
         } else {
           res.status(404).send({ res: "Password Incorrect" });
@@ -36,33 +37,5 @@ loginRouter.post("/", async (req, res) => {
     res.status(404).send({ res: "User Not Found, Try Signup First" });
   }
 });
-
-// loginRouter.post("/admin",async(req,res)=>{
-//   const { email, password, role } = req.body;
-//   if (!email || !password || !role) {
-//     return res.status(401).send({ res: "Please fill all required fields" });
-//   }
-//   const isExist = await UserModel.findOne({ email });
-//   if (isExist?.email) {
-//     try {
-//       const hashedPassword = isExist.password;
-//       bcrypt.compare(password, hashedPassword, (err, result) => {
-//         if (result) {
-//           const token = jwt.sign(
-//             { email: isExist.email, userID: isExist._id },
-//             "webtoken"
-//           );
-//           res.send({ res: `Hello ${isExist.name}`, name: isExist.name, token });
-//         } else {
-//           res.status(404).send({ res: "Password Incorrect" });
-//         }
-//       });
-//     } catch (err) {
-//       res.send({ res: err });
-//     }
-//   } else {
-//     res.status(404).send({ res: "User Not Found, Try Signup First" });
-//   }
-// })
 
 module.exports = { loginRouter };
